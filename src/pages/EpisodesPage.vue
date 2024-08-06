288e3bbbd11269da9c54b70dea9a5816f45320c9
Sinthix <ivo.ivan89@hotmail.com>
2024-08-06 15:59:05 +0100

    feat: adds EpisodesPage component

---
+++ src/pages/EpisodesPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <EpisodeDetail />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import EpisodeDetail from '../components/EpisodeDetail.vue';
+
+export default defineComponent({
+  components: {
+    EpisodeDetail
+  }
+});
+</script>
