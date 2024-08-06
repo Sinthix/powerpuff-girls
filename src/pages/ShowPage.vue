bb0c94a48e19550936e4b7c557eb32cfb18d6ab0
Sinthix <ivo.ivan89@hotmail.com>
2024-08-06 15:58:29 +0100

    feat: adds Showpage component

---
+++ src/pages/ShowPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <ShowDetail />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ShowDetail from '../components/ShowDetail.vue';
+
+export default defineComponent({
+  components: {
+    ShowDetail
+  }
+});
+</script>
